4bdbc26d83b3865a1ac36c60cb0ba0e6060b4ccc
perchik_air <1919ars@gmail.com>
2025-10-06 14:44:32 +0300

    more

--- draft_1.py
+++ draft_1.py
@@ -1,15 +1,22 @@
-from transformers import AutoTokenizer
+# from transformers import AutoTokenizer
+#
+# tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+#
+# # Get PAD token string
+# pad_token = tokenizer.pad_token
+# print(f"PAD token: '{pad_token}'")  # Output: '[PAD]'
+#
+# # Get PAD token ID
+# pad_token_id = tokenizer.pad_token_id
+# print(f"PAD token ID: {pad_token_id}")  # Output: 0
+#
+# # Alternative way to get PAD token ID
+# pad_id_alt = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
+# print(f"PAD token ID (alternative): {pad_id_alt}")  # Output: 0
+import time
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+from tqdm import tqdm
 
-# Get PAD token string
-pad_token = tokenizer.pad_token
-print(f"PAD token: '{pad_token}'")  # Output: '[PAD]'
+for review in tqdm(range(10)):
+    time.sleep(1)
 
-# Get PAD token ID
-pad_token_id = tokenizer.pad_token_id
-print(f"PAD token ID: {pad_token_id}")  # Output: 0
-
-# Alternative way to get PAD token ID
-pad_id_alt = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
-print(f"PAD token ID (alternative): {pad_id_alt}")  # Output: 0
--- p1_data_source.py
+++ p1_data_source.py
@@ -1,13 +1,21 @@
 import random
 import typing
 from collections import Counter
+from pathlib import Path
+
+from torchtext.vocab import vocab
+from torchtext.data.utils import get_tokenizer
+
 
 import numpy as np
+import torch
 from torch.utils.data import Dataset
 import  pandas as pd
 from transformers import AutoTokenizer
 from tqdm import tqdm
 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 general_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 class IMDBBertDataset(Dataset):
@@ -34,7 +42,8 @@ class IMDBBertDataset(Dataset):
         if ds_from is not None or ds_to is not None:
             self.ds = self.ds[ds_from:ds_to]
 
-        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        # self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        self.tokenizer = get_tokenizer('basic_english')
         self.counter = Counter()
         self.vocab = None
 
@@ -54,38 +63,30 @@ class IMDBBertDataset(Dataset):
         return len(self.df)
 
     def __getitem__(self, idx):
-        ...
+        item = self.df.iloc[idx]
 
-    def _find_optimal_sentence_length(self, lengths: typing.List[int]):
-        arr = np.array(lengths)
-        return int(np.percentile(arr, self.OPTIMAL_LENGTH_PERCENTILE))
+        inp = torch.Tensor(item[self.MASKED_INDICES_COLUMN]).long()
+        token_mask = torch.Tensor(item[self.TARGET_COLUMN]).long()
 
-    def _fill_vocab(self):
-        self.vocab = self.tokenizer.get_vocab()
+        mask_target = torch.Tensor(item[self.TARGET_COLUMN]).long()
+        mask_target = mask_target.masked_fill_(token_mask, 0)
 
-    def _create_item(self,first: typing.List[int], second: typing.List[int], target: int = 1):
-        update_first, first_mask = self._preprocess_sentence(first.copy())
-        update_second, second_mask = self._preprocess_sentence(second.copy())
-        nsp_sentence = update_first + [self.SEP] + update_second
-        nsp_indices = self.vocab.lookup_indices(nsp_sentence)
-        inverse_token_mask = first_mask + [True] + second_mask
+        attention_mask = (inp == self.vocab[self.PAD]).unsqueeze(0)
 
-    def _mask_sentence(self, sentence: typing.List[str]):
-        len_s = len(sentence)
-        inverse_token_mask = [True for _ in range(max(len_s, self.optimal_sentence_length))]
+        if item[self.NSP_TARGET_COLUMN] == 0:
+            t = [1, 0]
+        else:
+            t = [0, 1]
 
-        mask_amount = round(len_s * self.MASK_PERCENTAGE)
-        for _ in range(mask_amount):
-            i = random.randint(0, len_s - 1)
+        nsp_target = torch.Tensor(t)
 
-            if random.random() < 0.8:
-                sentence[i] = self.MASK
-            else:
-                j = random.randint(5, len(self.vocab) - 1)
-                sentence[i] = self.vocab.lookup_token(j)
-            inverse_token_mask[i] = False
-
-        return sentence, inverse_token_mask
+        return (
+            inp.to(device),
+            attention_mask.to(device),
+            token_mask.to(device),
+            mask_target.to(device),
+            nsp_target.to(device)
+        )
 
     def prepare_dataset(self) -> pd.DataFrame:
         sentences = []
@@ -98,6 +99,7 @@ class IMDBBertDataset(Dataset):
             sentences += review_sentences
             self._update_length(review_sentences, sentence_lens)
         self.optimal_sentence_length = self._find_optimal_sentence_length(sentence_lens)
+
         print('Created vocabulary')
         for sentence in tqdm(sentences):
             s = self.tokenizer(sentence)
@@ -120,4 +122,116 @@ class IMDBBertDataset(Dataset):
                     nsp.append(self._create_item(first, second, 0))
 
         df = pd.DataFrame(nsp, columns=self.columns)
+        return df
+
+    def _update_length(self, sentence: typing.List[str], lengths: typing.List[int]):
+        for v in sentence:
+            l = len(v.split())
+            lengths.append(l)
+        return lengths
+
+    def _find_optimal_sentence_length(self, lengths: typing.List[int]):
+        arr = np.array(lengths)
+        return int(np.percentile(arr, self.OPTIMAL_LENGTH_PERCENTILE))
+
+    def _fill_vocab(self):
+        self.vocab = vocab(self.counter, min_freq=2)
+
+        self.vocab.insert_token(self.CLS, 0)
+        self.vocab.insert_token(self.PAD, 1)
+        self.vocab.insert_token(self.MASK, 2)
+        self.vocab.insert_token(self.SEP, 3)
+        self.vocab.insert_token(self.UNK, 4)
+        self.vocab.set_default_index(4)
+
+    def _create_item(self,first: typing.List[int], second: typing.List[int], target: int = 1):
+        update_first, first_mask = self._preprocess_sentence(first.copy())
+        update_second, second_mask = self._preprocess_sentence(second.copy())
+        nsp_sentence = update_first + [self.SEP] + update_second
+        nsp_indices = self.vocab.lookup_indices(nsp_sentence)
+        inverse_token_mask = first_mask + [True] + second_mask
+
+        first, _ = self._preprocess_sentence(first.copy(), should_mask=False)
+        second, _ = self._preprocess_sentence(first.copy(), should_mask=False)
+        original_nsp_sentence = first + [self.SEP] + second
+        original_nsp_indices = self.vocab.lookup_indices(original_nsp_sentence)
+
+        if self.should_include_text:
+            return (
+                nsp_sentence,
+                nsp_indices,
+                original_nsp_sentence,
+                original_nsp_indices,
+                inverse_token_mask,
+                target
+            )
+        else:
+            return (
+                nsp_indices,
+                original_nsp_indices,
+                inverse_token_mask,
+                target
+            )
+
+    def _select_false_nsp_sentences(self, sentences: typing.List[str]):
+        sentences_len = len(sentences)
+        sentence_index = random.randint(0, sentences_len - 1)
+        next_sentence_index = random.randint(0, sentences_len - 1)
+
+        # to be sure that it's not real next sentence
+        while next_sentence_index == sentences_len + 1:
+            next_sentence_index = random.randint(0, sentences_len - 1)
+
+        return sentences[sentence_index], sentences[next_sentence_index]
+
+    def preprocess_sentence(self, sentence: typing.List[str], should_mask: bool = True):
+        inverse_token_mask = None
+        if should_mask:
+            sentence, inverse_token_mask = self._mask_sentence(sentence)
+        sentence, inverse_token_mask = self._pad_sentence([self.CLS] + sentence, [True] + inverse_token_mask)
+
+        return sentence, inverse_token_mask
+
+    def _mask_sentence(self, sentence: typing.List[str]):
+        len_s = len(sentence)
+        inverse_token_mask = [True for _ in range(max(len_s, self.optimal_sentence_length))]
+
+        mask_amount = round(len_s * self.MASK_PERCENTAGE)
+        for _ in range(mask_amount):
+            i = random.randint(0, len_s - 1)
+
+            if random.random() < 0.8:
+                sentence[i] = self.MASK
+            else:
+                j = random.randint(5, len(self.vocab) - 1)
+                sentence[i] = self.vocab.lookup_token(j)
+            inverse_token_mask[i] = False
+
+        return sentence, inverse_token_mask
+
+    def _pad_sentence(self, sentence: typing.List[str], inverse_token_mask: typing.List[bool] = None):
+        len_s = len(sentence)
+
+        if len_s >= self.optimal_sentence_length:
+            s = sentence[:self.optimal_sentence_length]
+        else:
+            s = sentence + [self.PAD] * (self.optimal_sentence_length - len_s)
+
+        if inverse_token_mask:
+            len_m = len(inverse_token_mask)
+            if len_m >= self.optimal_sentence_length:
+                inverse_token_mask = inverse_token_mask[:self.optimal_sentence_length]
+            else:
+                inverse_token_mask = inverse_token_mask + [True] * (self.optimal_sentence_length - len_m)
+        return s, inverse_token_mask
+
+
+if __name__ == '__main__':
+    BASE_DIR = Path(__file__).resolve().parent.parent
+
+    ds = IMDBBertDataset(BASE_DIR.joinpath('data/imdb.cvs'), ds_from=0, ds_to=50000, should_include_text=True)
+    print(ds.df)
+
+
+
 
